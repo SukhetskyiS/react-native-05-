738c82b6a533cde4be4a43f960e83d169a7f702b
SukhetskyiS <medpredin@gmail.com>
2023-02-17 01:33:49 +0200

    333

--- App.js
+++ App.js
@@ -1,65 +1,64 @@
-import { StatusBar } from 'expo-status-bar'
-import { RegistrationScreen } from './Screens/Auth/RegistrationScreen'
-import { LoginScreen } from './Screens/Auth/LoginScreen'
-import { HomeScreen } from './Screens/Home/HomeScreen'
-import { useCallback } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { RegistrationScreen } from "./Screens/Auth/RegistrationScreen";
+import { LoginScreen } from "./Screens/Auth/LoginScreen";
+import { HomeScreen } from "./Screens/Home/HomeScreen";
+import { useCallback } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { useFonts } from 'expo-font'
-import * as SplashScreen from 'expo-splash-screen'
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
-SplashScreen.preventAutoHideAsync()
-const Stack = createNativeStackNavigator()
+SplashScreen.preventAutoHideAsync();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-    const [fontsLoaded] = useFonts({
-        'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
-        'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
-    })
+  const [fontsLoaded] = useFonts({
+    "Roboto-Medium": require("./assets/Fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./assets/Fonts/Roboto-Regular.ttf"),
+  });
 
-    const onLayoutRootView = useCallback(async () => {
-        if (fontsLoaded) {
-            await SplashScreen.hideAsync()
-        }
-    }, [fontsLoaded])
-
-    if (!fontsLoaded) {
-        return null
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
     }
+  }, [fontsLoaded]);
 
-    return (
-        <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                    headerShown: false,
-                }}
-            >
-                <Stack.Screen name="Registration">
-                    {(props) => (
-                        <RegistrationScreen
-                            {...props}
-                            onLayoutRootView={onLayoutRootView}
-                        />
-                    )}
-                </Stack.Screen>
-                <Stack.Screen name="Login">
-                    {(props) => (
-                        <LoginScreen
-                            {...props}
-                            onLayoutRootView={onLayoutRootView}
-                        />
-                    )}
-                </Stack.Screen>
-                <Stack.Screen name="Home">
-                    {(props) => (
-                        <HomeScreen
-                            {...props}
-                            onLayoutRootView={onLayoutRootView}
-                        />
-                    )}
-                </Stack.Screen>
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Registration">
+          {(props) => (
+            <RegistrationScreen
+              {...props}
+              onLayoutRootView={onLayoutRootView}
+            />
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="Login">
+          {(props) => (
+            <LoginScreen
+              {...props}
+              onLayoutRootView={onLayoutRootView}
+            />
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="Home">
+          {(props) => (
+            <HomeScreen
+              {...props}
+              onLayoutRootView={onLayoutRootView}
+            />
+          )}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
